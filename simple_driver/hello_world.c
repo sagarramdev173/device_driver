e18fbd496a2380408f1c1ff653b0b3bff16fef1a
sagar sagar <sagar.sagar@oracle.com>
2023-06-26 15:34:03 +0530

    simple hello wolrd program

---
+++ simple_driver/hello_world.c
@@ -0,0 +1,12 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int __init my_init(void) {
+  printk("hello world\n");
+  return 0;
+}
+
+static void __exit my_exit(void) { printk("bye cruel wolrd\n"); }
+
+module_init(my_init);
+module_exit(my_exit);
