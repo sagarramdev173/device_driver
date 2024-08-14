8470cbd1e867128bf13075163317c003c243cfe1
Sagar Sagar <sagarramdev173@gmail.com>
2024-08-14 18:28:49 +0530

    fixed interrupt driver

--- 8_interrupt_driver/driver.c
+++ 8_interrupt_driver/driver.c
@@ -1,150 +1,84 @@
-// TODO: This driver is currently not useful it crashes need to fix 
-
-#define DRIVER_PREFIX "demo_driver: " 
-#define pr_fmt(fmt) DRIVER_PREFIX  fmt
-
-#include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include<linux/slab.h>   
-#include<linux/uaccess.h>
-#include<linux/sysfs.h> 
-#include<linux/kobject.h>
+#include <linux/init.h>
+#include <linux/gpio.h>
 #include <linux/interrupt.h>
-#include <asm/io.h>
-#include <linux/workqueue.h>
-#include <linux/interrupt.h>
-#include <asm/hw_irq.h>
 
-
-// preprocessor to make work easy
-#define INIT_FUNC static int __init
-#define EXIT_FUNC static void __exit
-#define SUCCESS 0;
-#define FAILURE -1;
-
-// information required for device 
-dev_t dev=0;
-static struct cdev demo_cdev;
-static struct class *dev_class;
-
-// driver file operations
-static int my_open(struct inode *inode, struct file *file){
-    pr_info("device has been opened\n");
-    return SUCCESS
-}
-
-static int my_release(struct inode *inode, struct file *file){
-    pr_info("device has been released\n");
-    return SUCCESS
-}
-
-static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
-    struct irq_desc *desc ;
-    pr_info("device read has been started\n");
-    /*
-        So if we want to raise an interrupt IRQ11, programmatically we have to add 11 to a vector of IRQ0.
-        0x20 + 0x10 + 11 = 0x3B (59 in Decimal).
-        Hence, executing “asm("int $0x3B")“, will raise interrupt IRQ 11.
-    */
-    // desc  = irq_to_desc(11);
-    // if(desc == NULL){
-    //     pr_info("IRQ not found\n");
-    //     return FAILURE
-    // }
-    // __this_cpu_write(vector_irq[59],desc);
-    // asm("int $0x3B");
-    asm("int $0x50");
-    return SUCCESS
-}
-static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off){
-    pr_info("device write has been started\n");
-    return SUCCESS
-}
-
-// file operation structure
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = my_open,
-    .release = my_release,
-    .read = my_read,
-    .write = my_write
-};
-// Interrupt Handler
-# define MY_IRQ_NO 12
-static irqreturn_t demo_irq_handler(int irq, void *dev_id){
-    pr_info("Shared IRQ: Interrupt Occurred");
-    return IRQ_HANDLED;
-}
-static int irq_requested = 0; // Flag to track IRQ allocation
-
-INIT_FUNC driver_start(void){
-    pr_info("device is being inserted\n");
-
-    // allocating the major number
-    if(alloc_chrdev_region(&dev,0,1,"demo_device")<0){
-        pr_info("Cannot allocate major number\n");
-        return FAILURE
-    }
-    pr_info("major =%d mindor =%d", MAJOR(dev),MINOR(dev));
-    
-    //creating cdev structure
-    cdev_init(&demo_cdev,&fops);
-
-    // adding cdev to the system
-    if(cdev_add(&demo_cdev,dev,1)<0){
-        pr_info("cannot add the device to the system\n");
-        goto r_class;
-    }
-
-    // creating device class
-    dev_class= class_create(THIS_MODULE,"demo_class");
-    if(dev_class == NULL){
-        pr_info("class creation failed\n");
-        goto r_class;
-    }
-
-    //setting up device node for normal user
-    if(device_create(dev_class,NULL,dev,NULL,"demo_device") == NULL){
-        pr_info("cannot create device file\n");
-        goto r_device;
-    }
-   
-    if(request_irq(MY_IRQ_NO, demo_irq_handler, IRQF_PROBE_SHARED, "demo_driver", NULL)){
-        pr_info("cannot register IRQ \n");
-        goto free_my_irq;
-    }
-    irq_requested = 1; // Mark IRQ as requested successfully
-    pr_info("device has been inserted \n");
-    return SUCCESS
-free_my_irq:
-    free_irq(MY_IRQ_NO, NULL);  
-r_device:
-    class_destroy(dev_class);
-r_class:
-    unregister_chrdev_region(dev,1);
-    cdev_del(&demo_cdev);
-    return FAILURE
-}
-
-
-EXIT_FUNC driver_end(void){
-    pr_info("device is being removed\n");
-     if (irq_requested) {
-        free_irq(MY_IRQ_NO, &demo_cdev);
-        irq_requested = 0;
-    }
-    device_destroy(dev_class,dev);
-    class_destroy(dev_class);
-    unregister_chrdev_region(dev,1);
-    cdev_del(&demo_cdev);   
-    pr_info("device has been removed\n");
-}
-
-module_init(driver_start);
-module_exit(driver_end);
+/* Meta Information */
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sagar Sagar");
+MODULE_DESCRIPTION("A simple LKM for a gpio interrupt");
+
+/** variable contains pin number o interrupt controller to which GPIO 17 is mapped to */
+unsigned int irq_number;
+int gpio_input  = 528;
+int gpio_output = 532;
+static int value = 0;
+/*
+ *
+ * @brief Interrupt service routine is called, when interrupt is triggered
+ */
+static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
+{
+	printk("gpio_irq: Interrupt was triggered and ISR was called!\n");
+  value = (value == 0 ) ? 1 :0;
+  gpio_set_value(gpio_output,value);
+	return IRQ_HANDLED;
+}
+
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init ModuleInit(void) {
+	printk("qpio_irq: Loading module... ");
+
+	/* Setup the gpio */
+	if(gpio_request(gpio_input, "rpi-gpio-16")) {
+		printk("Error!\nCan not allocate GPIO %d\n", gpio_input);
+		return -1;
+	}
+
+	if(gpio_request(gpio_output, "rpi-gpio-20")) {
+		printk("Error!\nCan not allocate GPIO %d\n", gpio_input);
+		return -1;
+	}
+	/* Set GPIO 17 direction */
+	if(gpio_direction_input(gpio_input)) {
+		printk("Error!\nCan not set GPIO 17 to input!\n");
+		gpio_free(gpio_input);
+		return -1;
+	}
+
+	if(gpio_direction_output(gpio_output, 0)) {
+		printk("Error!\nCan not set GPIO 20 to input!\n");
+		gpio_free(gpio_output);
+		gpio_free(gpio_input);
+		return -1;
+	}
+	/* Setup the interrupt */
+	irq_number = gpio_to_irq(gpio_input);
+
+	if(request_irq(irq_number, gpio_irq_handler, IRQF_TRIGGER_RISING, "my_gpio_irq", NULL) != 0){
+		printk("Error!\nCan not request interrupt nr.: %d\n", irq_number);
+		gpio_free(gpio_input);
+		return -1;
+	}
+
+	printk("Done!\n");
+	printk("GPIO %d is mapped to IRQ Nr.: %d\n", gpio_input,irq_number);
+	return 0;
+}
+
+/**
+ * @brief This function is called, when the module is removed from the kernel
+ */
+static void __exit ModuleExit(void) {
+	printk("gpio_irq: Unloading module... ");
+	free_irq(irq_number, NULL);
+  gpio_set_value(gpio_output,0);
+	gpio_free(gpio_output);
+	gpio_free(gpio_input);
+
+}
+
+module_init(ModuleInit);
+module_exit(ModuleExit);
